b8a107b85055b1a4befde6c0ad4da2a5733f9f91
Leonard Tng <leonardtng@gmail.com>
2021-02-13 21:20:47 +0800

    Added some sentences

--- src/@constants/sentenceBank.ts
+++ src/@constants/sentenceBank.ts
@@ -3,7 +3,9 @@ export const defaultSentencesSet1: Array<string> = [
   'My heart skips a beat when I think of you. ',
   'You are essential to me. ',
   'I want to take you far far away. ',
-  'You are my favorite distraction. '
+  'You are my favorite distraction. ',
+  'Your love makes my world go round. ',
+
 ]
 
 export const defaultSentencesSet2: Array<string> = [
@@ -18,11 +20,15 @@ export const defaultSentencesSet2: Array<string> = [
 export const forHerSentences: Array<string> = [
   'The way you look at me makes me feel strong and confident. ',
   'I crown you the Queen of my heart. ',
-  'Every time I see you I fall in love all over again. '
+  'Every time I see you I fall in love all over again. ',
+  'You bring more to me than I ever wanted. ',
+  'You are the girl of my dreams. ',
 ]
 
 export const forHimSentences: Array<string> = [
   'When I am in your arms, I feel safe, I feel loved and I feel like I am going to be protected from all the stones life can throw. ',
   'Thank you for making me feel like the most beautiful person in the world. ',
-  'You should teach other men how to love their partners and make them feel special because you have it down to an art. '
+  'You should teach other men how to love their partners and make them feel special because you have it down to an art. ',
+  'You are my prince charming. ',
+  'You are the man of my dreams. '
 ]
